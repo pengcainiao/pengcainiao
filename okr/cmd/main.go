51ac9d3477ba56021c5bbd6d0dc1b00460fdbaaf
longhui peng <96058253+pengcainiao@users.noreply.github.com>
2023-04-27 00:47:30 +0800

    mod 调试完成

--- okr/cmd/main.go
+++ okr/cmd/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"github.com/pengcainiao/pengcainiao/okr/internal"
+	"github.com/pengcainiao/zero/rest/httprouter"
+)
+
 func main() {
 	httprouter.RecoveryMainAsync(func() {
 		internal.Setup()
